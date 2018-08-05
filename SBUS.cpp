75b3a2142bdab7063b1ac501a7e313a6a4481f37
John Poltrack <jlpoltrack@gmail.com>
2018-08-05 16:33:43 -0400

    Eliminate Timer Code

--- SBUS.cpp
+++ SBUS.cpp
@@ -6,17 +6,6 @@ void SBUS::begin() {
 }
 
 void SBUS::begin(bool useTimer) {
-	if (useTimer) {
-		noInterrupts();
-		TCCR2A  = 0;
-		TCCR2B  = 0;
-		TCNT2   = 0;
-		OCR2A   = 249;
-		TCCR2A |= (1 << WGM21);
-		TCCR2B |= (1 << CS22);
-		TIMSK2 |= (1 << OCIE2A);
-		interrupts();
-	}
 
 	for (byte i = 0; i<18; i++) {
 		_channels[i]      = 0;
